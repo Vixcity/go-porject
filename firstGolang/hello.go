9a25da29f13bbaf8473dd2ef9669d518412896a7
Vixcity <2091283625@qq.com>
2021-09-24 15:39:12 +0800

    hello Go

---
+++ firstGolang/hello.go
@@ -0,0 +1,16 @@
+package main // 当前程序的包名
+/*
+一行代码的引入方式
+import "fmt"
+import "time"
+*/
+
+import (
+	"fmt"
+	"time"
+)
+
+func main()  {//函数的左花括号一定要和函数在同一行
+	fmt.Println("hello GO!")
+	time.Sleep(1 * time.Second)
+}
